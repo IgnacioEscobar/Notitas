aee499066cc96991d134cc95d2eacf141933c5a6
GabrielRuderman <gabriel_ruderman@hotmail.com>
2018-07-08 22:35:15 -0300

    Agrego getters y setters en Tarea

--- src/main/java/model/Tarea.java
+++ src/main/java/model/Tarea.java
@@ -3,9 +3,27 @@ package model;
 import java.util.List;
 
 public class Tarea {
+	private String descripcion;
 	private List<Nota> notas;
 	private Nota notaActual;
 	
+	public Tarea(String descripcion) {
+		super();
+		this.descripcion = descripcion;
+	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public void setNotaActual(Nota notaActual) {
+		this.notaActual = notaActual;
+	}
+
 	public Nota getNotaActual() {
 		return notaActual;
 	}
