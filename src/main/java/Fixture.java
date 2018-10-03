16cb24cd875a8469ed5049aaed4c9d71424f65ad
IgnacioEscobar <ignacionicolas.e.c@gmail.com>
2018-10-03 01:32:02 -0300

    :Alembic: Peticion y parseo de tareas

--- src/main/java/Fixture.java
+++ src/main/java/Fixture.java
@@ -1,4 +1,5 @@
 import client.RestClient;
+import json.Asignaciones;
 import json.JSONParser;
 import model.Alumno;
 import model.Tarea;
@@ -8,19 +9,10 @@ public class Fixture {
 	public static void init() {
         
         RestClient cliente = new RestClient("http://notitas.herokuapp.com/");
-        JSONParser parser = new JSONParser();
-        Alumno alumno = parser.objetoDesdeString(cliente.getResource("student"), Alumno.class);
+        Alumno alumno = JSONParser.objetoDesdeString(cliente.getResource("student"), Alumno.class);
         Alumno.setInstance(alumno);
-        Tarea tarea = new Tarea("1 Parcial");
-        tarea.evaluar("7");
-        alumno.asignarTarea(tarea);
-        Tarea tarea2 = new Tarea("2 Parcial");
-        tarea2.evaluar("4");
-        alumno.asignarTarea(tarea2);
-        Tarea tarea3 = new Tarea("Trabajo anual");
-        tarea3.evaluar("B");
-        alumno.asignarTarea(tarea3);
-        
+        Asignaciones asignaciones = JSONParser.objetoDesdeString(cliente.getResource("student/assignments"), Asignaciones.class);
+        Alumno.getInstance().setTareas(asignaciones.getTareas());
 	}
 
 }
