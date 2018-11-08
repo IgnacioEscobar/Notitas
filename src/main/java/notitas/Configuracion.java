618d49d813584438d9bc310079664fab991a55a4
IgnacioEscobar <ignacionicolas.e.c@gmail.com>
2018-11-07 22:17:55 -0300

    Cambio de token en cliente

--- src/main/java/notitas/Configuracion.java
+++ src/main/java/notitas/Configuracion.java
@@ -4,6 +4,5 @@ import notitas.client.NotitasClient;
 
 public class Configuracion {
     public static NotitasClient cliente;
-    public static String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIxMTEyMjIzMzMiLCJybmQiOiJ5SXNmZFIwN2lIR3BRRmVjYU9KT2VRPT0ifQ.9pVJGUXhrJPQ-TptNCt971l0h_1dWqWgMrHAWXJchho";
-    //    eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoxfQ.MzzE0CbwNB09Ig1ZY42Q4ss0Hjcmqj-P3tcjaq8ZPLk
+    public static String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoxfQ.MzzE0CbwNB09Ig1ZY42Q4ss0Hjcmqj-P3tcjaq8ZPLk";
 }
--- src/main/java/notitas/server/NotitasServer.java
+++ src/main/java/notitas/server/NotitasServer.java
@@ -3,30 +3,18 @@ package notitas.server;
 import spark.Spark;
 import spark.debug.DebugScreen;
 
+import static spark.Spark.before;
+
 public class NotitasServer {
 	public static void main(String[] args) {
 		Fixture.init();
-		Repositorio repo = Repositorio.getInstance();
-
 		Spark.port(9000);
 		DebugScreen.enableDebugScreen();
-		Spark.get("/", (req, res) -> "<marquee>Holiis!</marquee>");
-		Spark.get("/student", Controller::getAlumnoAsJSON);
-		Spark.get("/student/assignments", Controller::getAsignacionesAsJSON);
-		Spark.post("/student",Controller::setAlumno);
 
-
-//		SecurityService securityService = new SecurityService("god");
-		
-		
-		/*Spark.before((req, res) -> {
-			try {
-				Long userId = securityService.user(req.headers("Authorization").replace("Bearer ", ""));
-				//Hacer algo con el id...
-			} catch (Exception e) {
-				Spark.halt(401, "<h1><a href='https://www.youtube.com/watch?v=0Jx8Eay5fWQ'>Hack me </a></h1><br/><br/><br/><a href='https://www.youtube.com/watch?v=PtLmEARfStE'> El aleph </a>");
-			}
-		});*/
+		Spark.get ("/"						, (req, res) -> "<marquee>Holiis!</marquee>");
+		Spark.get ("/student"				, Controller::getAlumnoAsJSON);
+		Spark.get ("/student/assignments"	, Controller::getAsignacionesAsJSON);
+		Spark.post("/student"				, Controller::setAlumno);
 	}
 
 }
