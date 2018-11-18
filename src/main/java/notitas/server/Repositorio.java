a02cae1f1cce8ea6aed67f894480e9e462516e37
GabrielRuderman <gabriel_ruderman@hotmail.com>
2018-11-18 02:12:38 -0300

    Query para cargar alumno actual

--- src/main/java/notitas/server/Repositorio.java
+++ src/main/java/notitas/server/Repositorio.java
@@ -1,28 +1,49 @@
 package notitas.server;
 
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.uqbarproject.jpa.java8.extras.PerThreadEntityManagers;
+
 import notitas.json.Asignaciones;
 import notitas.model.Alumno;
 
-class Repositorio {
+public class Repositorio {
     private static Repositorio ourInstance = new Repositorio();
     private Alumno alumno;
 
     private Repositorio() {
     }
 
-    static Repositorio getInstance() {
+    public static Repositorio getInstance() {
         return ourInstance;
     }
 
-    Alumno getAlumno(Long id) {
+    Alumno getAlumno(Long userID) {
+    	EntityManager manager =  PerThreadEntityManagers.getEntityManager();
+    	
+    	Query query= manager.createNativeQuery("select *\r\n" + 
+		 		"from alumno\r\n" + 
+		 		"where id = ?1");
+	      
+		query.setParameter(1, userID);
+		 
+		List<Alumno> alumnos =  query.getResultList();
+		
+		alumno = alumnos.get(0);
+		
         return alumno;
     }
 
-    void setAlumno(Long id, Alumno alumno) {
+    public void setAlumno(Long id, Alumno alumno) {
         this.alumno = alumno;
     }
 
     Asignaciones getAsignaciones(Long id) {
+    	System.out.println("NO ESTA HECHO");
         return new Asignaciones(alumno.getTareas());
     }
 
