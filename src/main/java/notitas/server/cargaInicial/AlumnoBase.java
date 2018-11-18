d763e2b77d0de73febe66b10a6a78201b80d8b46
GabrielRuderman <gabriel_ruderman@hotmail.com>
2018-11-18 02:39:56 -0300

    Agrego transactions a la carga inicial y creo las tablas

--- src/main/java/notitas/server/cargaInicial/AlumnoBase.java
+++ src/main/java/notitas/server/cargaInicial/AlumnoBase.java
@@ -1,15 +1,30 @@
 package notitas.server.cargaInicial;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
-
+import javax.persistence.EntityTransaction;
 import org.uqbarproject.jpa.java8.extras.PerThreadEntityManagers;
 
 public class AlumnoBase {
 	public static void init() {
-		EntityManager manager =  PerThreadEntityManagers.getEntityManager();
 		
-    	manager.createNativeQuery("insert into alumno (userID, nombre, apellido, legajo, usuarioGitHub)\r\n" + 
-		 		"values(1, 'Gabriel', 'Ruderman', 1587973, 'gabrielruderman')").executeUpdate();
+		EntityTransaction trans = null;
+		try {
+			EntityManager em = PerThreadEntityManagers.getEntityManager();
+			trans = em.getTransaction();
+			trans.begin();
+			
+			em.createNativeQuery("create table alumno (userID (number), nombre (char(40)), apellido (char(40)),"
+					+ "legajo (number), usuarioGitHub (char(40))").executeUpdate();
+			em.createNativeQuery("insert into alumno (userID, nombre, apellido, legajo, usuarioGitHub)\r\n" + 
+			 		"values(1, 'Gabriel', 'Ruderman', 1587973, 'gabrielruderman')").executeUpdate();
+
+			trans.commit();
+		}
+		catch (Throwable e) {
+			if (trans != null && trans.isActive()) {
+				trans.rollback();
+			}
+			throw e;
+		}
     }
 }
--- src/main/java/notitas/server/cargaInicial/TareasBase.java
+++ src/main/java/notitas/server/cargaInicial/TareasBase.java
@@ -1,17 +1,30 @@
 package notitas.server.cargaInicial;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
-
+import javax.persistence.EntityTransaction;
 import org.uqbarproject.jpa.java8.extras.PerThreadEntityManagers;
 
-import notitas.model.Tarea;
-
 public class TareasBase {
-	public static void init() {
-		EntityManager manager =  PerThreadEntityManagers.getEntityManager();
-    	
-    	manager.createNativeQuery("insert into tarea (descripcion, notas, notaActual, alumno_id)\r\n" + 
-		 		"values('moduloB', 9, 9, 1)").executeUpdate();
-    }
+	public static void init() {	
+	
+		EntityTransaction trans = null;
+		try {
+			EntityManager em = PerThreadEntityManagers.getEntityManager();
+			trans = em.getTransaction();
+			trans.begin();
+			
+			em.createNativeQuery("create table tarea (id (number), descripcion (char(80)), notas (char(20)),"
+					+ "notaActual (char(20)),").executeUpdate();
+			em.createNativeQuery("insert into tarea (descripcion, notas, notaActual, alumno_id)\r\n" + 
+			 		"values('moduloB', 9, 9, 1)").executeUpdate();
+	
+			trans.commit();
+		}
+		catch (Throwable e) {
+			if (trans != null && trans.isActive()) {
+				trans.rollback();
+			}
+			throw e;
+		}
+	}
 }
